1881d7653cb0af61bc973ede693c98bd59112899
Klaus Boeing <Klaus Boeing@klausboeing.mshome.net>
2013-06-22 11:15:40 -0300

    Exemplo Conta Bancaria

---
+++ src/classesobjetos/exemplos/banco/Conta.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classesobjetos.exemplos.banco;
+
+/**
+ *
+ * @author Klaus Boeing
+ */
+public class Conta {
+
+    Integer numero;
+    String dono;
+    Double saldo;
+    Double limite;
+    
+    boolean saca(Double valor){
+        if(saldo >= valor){
+            saldo -= valor;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    boolean transfere(Conta contaDestino, Double valor){
+        if(saca(valor)){
+            contaDestino.deposita(valor);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    void deposita(Double valor){
+        saldo += valor;
+    }
+    
+}
