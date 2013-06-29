1b3b3775d68789e472ec06f30175c356c9add730
Klaus Boeing <Klaus Boeing@klausboeing.mshome.net>
2013-06-29 10:33:45 -0300

    Exemplo Departamento (Tell, don't ask)

---
+++ src/classesobjetos/exercicios/funcionario/Departamento.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classesobjetos.exercicios.funcionario;
+
+/**
+ *
+ * @author Klaus Boeing
+ */
+public class Departamento {
+    
+    String nome;
+    
+    Double percentualInsalubridade;
+    
+    Double valorBonus;
+ 
+    Double aplicaValorInsalubridade(Double salario){
+        return salario + (salario * (percentualInsalubridade /100 * 1)) + valorBonus;
+    }
+    
+}
