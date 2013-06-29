7155cb6fe4e857d334d89ede9ccd7500784dcf54
EDUTEC <EDUTEC@10.8.0.20>
2013-06-29 12:45:22 -0300

    Alterado

--- src/classesobjetos/exercicios/funcionario/TestaFuncionario.java
+++ src/classesobjetos/exercicios/funcionario/TestaFuncionario.java
@@ -10,36 +10,36 @@ package classesobjetos.exercicios.funcionario;
  */
 public class TestaFuncionario {
     public static void main(String[] args) {
-        Empresa emp = new Empresa();        
-        Funcionario[] func = new Funcionario[10];
-        funcionario.funcionario = func;
-        Funcionario funcionario = new Funcionario();
-        empresa.adicionaFuncionariosNaEmpresa(funcionario);
-        System.out.println("Nome do funcionário solicitado: " + empresa.imprimiEmpresaComFuncionarios(funcionario));
-    }
-}
-         
-        departamento.nome = "A";
-        departamento.percentualInsalubridade = 10.0;
-        departamento.valorBonus = 100.0;
-
-        carlos.departamento = departamento;
-        
-        Data dataEntrada = new Data();
-        
-        dataEntrada.dia = 12;
-        dataEntrada.mes = 6;
-        dataEntrada.ano = 2013;
-        
-        carlos.dataEntrada = dataEntrada;
-        
-        carlos.mostra();
-        
-        System.out.println("---------------------");
-        
-        carlos.recebeAumento(200.0);
-        
-        carlos.mostra();
+//        Empresa emp = new Empresa();        
+//        Funcionario[] func = new Funcionario[10];
+//        funcionario.funcionario = func;
+//        Funcionario funcionario = new Funcionario();
+//        empresa.adicionaFuncionariosNaEmpresa(funcionario);
+//        System.out.println("Nome do funcionário solicitado: " + empresa.imprimiEmpresaComFuncionarios(funcionario));
+//    }
+//}
+//         
+//        departamento.nome = "A";
+//        departamento.percentualInsalubridade = 10.0;
+//        departamento.valorBonus = 100.0;
+//
+//        carlos.departamento = departamento;
+//        
+//        Data dataEntrada = new Data();
+//        
+//        dataEntrada.dia = 12;
+//        dataEntrada.mes = 6;
+//        dataEntrada.ano = 2013;
+//        
+//        carlos.dataEntrada = dataEntrada;
+//        
+//        carlos.mostra();
+//        
+//        System.out.println("---------------------");
+//        
+//        carlos.recebeAumento(200.0);
+//        
+//        carlos.mostra();
     }
     
 }
